ecac6db0e063de787370b4f64d23624ed159aaa6
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-22 19:11:59 +0900

    ✨ provider 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { RoomGateway } from "./room/room.gateway";
+import { UserService } from "./data/user/user.service";
+import { AppGateway } from "./app.gateway";
 
 @Module({
    imports: [],
    controllers: [AppController],
-   providers: [AppService],
+   providers: [AppService, RoomGateway, UserService, AppGateway],
 })
 export class AppModule {}
