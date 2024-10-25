66760b5cec5a5bed55fe8c599323a7f55d6f7f78
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-25 19:11:06 +0900

    🔧 Swagger 설정 및 prefix 설정

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
    const app = await NestFactory.create(AppModule);
+   app.setGlobalPrefix("api"); // 등록된 모든 경로의 접두사 api로 설정
+
+   // swagger 설정
+   const config = new DocumentBuilder()
+      .setTitle("KETCH-CATCH API")
+      .setDescription("실시간 그림 그리기 게임 API 문서")
+      .setVersion("1.0")
+      .build();
+   const documentFactory = () => SwaggerModule.createDocument(app, config);
+   SwaggerModule.setup("api-docs", app, documentFactory);
    app.useGlobalPipes(
       new ValidationPipe({
          forbidUnknownValues: true,
