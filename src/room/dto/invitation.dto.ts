fb68ac2dfe99a0529b9af8ed0b4c07eaba72407d
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-26 17:12:11 +0900

    🚚 파일명 수정 password.dto.ts -> invitation.dto.ts 📝초대 코드 및 비밀번호 dto 추가

--- src/room/dto/invitation.dto.ts
+++ src/room/dto/invitation.dto.ts
@@ -1,7 +1,7 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, PickType } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
-export class passwordDto {
+export class invitationDto {
    @ApiProperty({ description: "초대 코드" })
    @IsString({ message: "초대 코드는 문자열입니다." })
    @IsNotEmpty({ message: "초대 코드는 빈 값이 아닙니다." })
@@ -12,3 +12,7 @@ export class passwordDto {
    @IsNotEmpty({ message: "비밀번호는 빈 값이 아닙니다." })
    readonly password: string;
 }
+
+export class invitationCodeDto extends PickType(invitationDto, ["invitationCode"] as const) {}
+
+export class passwordDto extends PickType(invitationDto, ["password"] as const) {}
