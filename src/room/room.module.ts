0274ef7b24e600d145d38e9e7fa89149425d1728
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-28 18:33:36 +0900

    ✨ gateway 추가(for 예외처리)

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -3,10 +3,11 @@ import { RoomService } from "./room.service";
 import { RoomGateway } from "./room.gateway";
 import { DataModule } from "src/data/data.module";
 import { RoomController } from "./room.controller";
+import { AppGateway } from "src/app.gateway";
 
 @Module({
    imports: [DataModule],
-   providers: [RoomService, RoomGateway],
+   providers: [RoomService, RoomGateway, AppGateway],
    controllers: [RoomController],
    exports: [RoomService],
 })
