0b969469c1f581498ea97c0a4a3e8dc260d9dcbd
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-31 23:47:19 +0900

    🔧 서비스 로직 바뀜에 따라 코드 수정 ⚰️ 불필요한 코드 제거

--- src/room/room.gateway.ts
+++ src/room/room.gateway.ts
@@ -10,8 +10,8 @@ import { roomSettingDto } from "src/room/dto/room-setting.dto";
 import { userDto } from "src/room/dto/user.dto";
 import { RoomService } from "./room.service";
 import { RoomSettingService } from "src/data/room/room-setting.service";
-import { UserService } from "src/data/user/user.service";
 import { passwordDto } from "./dto/password.dto";
+import { GameState } from "./itf/gameState.interface";
 
 @WebSocketGateway({ namespace: "ketch-catch" })
 export class RoomGateway implements OnGatewayConnection {
@@ -20,7 +20,6 @@ export class RoomGateway implements OnGatewayConnection {
    constructor(
       private roomService: RoomService,
       private roomSettingService: RoomSettingService,
-      private userService: UserService,
    ) {}
 
    // 클라이언트 접속 시
@@ -31,7 +30,7 @@ export class RoomGateway implements OnGatewayConnection {
    // 방 생성
    @SubscribeMessage("create-room")
    async createRoom(socket: Socket, data: { userData: userDto; roomSetting: roomSettingDto }) {
-      // this.roomService.exitRoom(socket); // 기존에 있던 방 나가기
+      this.roomService.exitRoom(socket); // 기존에 있던 방 나가기
       const { userData, roomSetting } = data;
       const { roomId, users } = await this.roomService.createRoom(socket.id, userData, roomSetting);
       if (roomId === undefined || users === undefined) return; // 오류 발생 시
@@ -40,14 +39,14 @@ export class RoomGateway implements OnGatewayConnection {
       // room 입장
       socket.join(roomId);
 
-      this.server.emit("notice", message); // 입장 메시지 보냄
-      this.server.emit("pending-room", users); // roomId 방에 있는 유저들 정보 보냄
+      this.server.to(roomId).emit("notice", { message }); // 입장 메시지 보냄
+      this.server.to(roomId).emit("pending-room", users); // roomId 방에 있는 유저들 정보 보냄
    }
 
    // 랜덤 룸 입장
    @SubscribeMessage("entrance-random-room")
    async randomRoom(socket: Socket, userData: userDto) {
-      // this.roomService.exitRoom(socket); // 기존에 있던 방 나가기
+      this.roomService.exitRoom(socket); // 기존에 있던 방 나가기
       // Room List 복사
       let roomList = this.roomSettingService.room.map((room) => {
          return {
@@ -63,14 +62,14 @@ export class RoomGateway implements OnGatewayConnection {
       const message = `${userData.nickname}님이 입장하셨습니다.`;
 
       socket.join(roomId);
-      this.server.to(roomId).emit("notice", message); // 입장 메시지 보냄
+      this.server.to(roomId).emit("notice", { message }); // 입장 메시지 보냄
       this.server.to(roomId).emit("pending-room", users); // roomId 방에 있는 유저들 정보 보냄
    }
 
    //    // 초대 코드 입장
    @SubscribeMessage("entrance-invited-room")
    async invitedRoom(socket: Socket, data: { userData: userDto; authenticationCode: string; password: passwordDto }) {
-      // this.roomService.exitRoom(socket); // 기존에 있던 방 나가기
+      this.roomService.exitRoom(socket); // 기존에 있던 방 나가기
       const { userData, authenticationCode, password } = data;
       const { roomId, users } = await this.roomService.invitedRoom(socket.id, userData, authenticationCode, password);
       if (data === undefined) return;
@@ -78,30 +77,28 @@ export class RoomGateway implements OnGatewayConnection {
       socket.join(roomId);
 
       // roomId 방에 있는 유저들 정보 보냄
-      this.server.emit("pending-room", users);
-      this.server.emit("notice", message);
+      this.server.to(roomId).emit("pending-room", users);
+      this.server.to(roomId).emit("notice", { message });
    }
 
-   //    // 방 수정
-   //    @SubscribeMessage("update-room")
-   //    async updateRoom(socket: Socket, roomSetting: roomSettingDto) {
-   //       const roomId = await this.roomService.updateRoom(socket.id, roomSetting);
-   //       if (roomId === undefined) return; // 오류 발생 시
+   // 방 수정
+   @SubscribeMessage("update-room")
+   async updateRoom(socket: Socket, roomSetting: roomSettingDto) {
+      const { roomId, users } = await this.roomService.updateRoom(socket.id, roomSetting);
+      if (roomId === undefined || users === undefined) return; // 오류 발생 시
 
-   //       this.server.to(roomId).emit("pending-room", { ...roomSetting });
-   //       this.server.to(roomId).emit("notice", { message: "방 설정이 수정되었습니다." });
-   //    }
+      this.server.to(roomId).emit("pending-room", users);
+      this.server.to(roomId).emit("notice", { message: "방 설정이 수정되었습니다." });
+   }
 
-   //    // 방 나가기
-   //    @SubscribeMessage("leave-room")
-   //    exitRoom(@ConnectedSocket() socket: Socket) {
-   //       const { roomId, nickname } = this.roomService.exitRoom(socket);
-   //       if (roomId === undefined || nickname === undefined) return;
+   // 방 나가기
+   @SubscribeMessage("leave-room")
+   exitRoom(@ConnectedSocket() socket: Socket) {
+      const { roomId, users, nickname } = this.roomService.exitRoom(socket);
+      if (roomId === undefined || users === undefined || nickname === undefined) return;
 
-   //       const users = this.roomService.pendingRoomUsers(roomId);
-
-   //       this.userService.deleteUser(socket.id); // 나간 유저 정보 지움
-   //       this.server.to(roomId).emit("notice", { message: `${nickname}님이 방을 나갔습니다.` });
-   //       this.server.to(roomId).emit("join-room", users);
-   //    }
+      const eventName = users.gameState === GameState.Pending ? "pending-room" : "game-room";
+      this.server.to(roomId).emit("notice", { message: `${nickname}님이 방을 나갔습니다.` });
+      this.server.to(roomId).emit(eventName, users);
+   }
 }
