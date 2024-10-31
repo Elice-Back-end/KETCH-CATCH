d2ad3eb5c3dcc33ea217a43d9b8a4d48e2ff2b92
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-31 19:58:41 +0900

    📝 property 제거

--- src/room/dto/user.dto.ts
+++ src/room/dto/user.dto.ts
@@ -7,10 +7,6 @@ export class userDto {
    @IsNotEmpty({ message: "닉네임은 빈 값이 아니어야 합니다." })
    readonly nickname: string;
 
-   @IsString({ message: "소켓 id는 문자열이어야 합니다." })
-   @IsNotEmpty({ message: "소켓 id는 빈 값이 아니어야 합니다." })
-   readonly socketId: string;
-
    @IsNotEmptyObject({ nullable: false }, { message: "캐릭터는 빈 값이 아니어야 합니다." })
    @IsObject({ message: "캐릭터는 object 타입이어야 합니다." })
    @ValidateNested() // 중첩된 객체에 대한 유효성 검사 활성화
