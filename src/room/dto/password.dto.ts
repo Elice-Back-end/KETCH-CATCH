fba5c14cc491cc399100db6276ee14a09a91aeec
[IoT3] 조수빈 <cdw3521@naver.com>
2024-10-31 19:53:29 +0900

    📝 property 삭제 및 dto 검증 조건 추가

--- src/room/dto/password.dto.ts
+++ src/room/dto/password.dto.ts
@@ -1,16 +1,8 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString, ValidateNested } from "class-validator";
-import { userDto } from "./user.dto";
-import { Type } from "class-transformer";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class passwordDto {
-   @ApiProperty({ description: "방 비밀번호" })
+   @IsOptional()
    @IsString({ message: "비밀번호는 문자열입니다." })
    @IsNotEmpty({ message: "비밀번호는 빈 값이 아닙니다." })
-   readonly password: string;
-
-   @ApiProperty({ description: "유저 정보" })
-   @ValidateNested() // 중첩된 객체에 대한 유효성 검사 활성화
-   @Type(() => userDto)
-   readonly userData: userDto;
+   readonly password: string | null;
 }
