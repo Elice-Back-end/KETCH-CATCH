49b4109054bead5b839c597b0c5477db52267ec4
[IoT3] 조수빈 <cdw3521@naver.com>
2024-11-04 22:13:18 +0900

    🔥코드 삭제

--- src/room/dto/user.dto.ts
+++ src/room/dto/user.dto.ts
@@ -1,15 +1,7 @@
-import { IsNotEmpty, IsNotEmptyObject, IsObject, IsString, ValidateNested } from "class-validator";
-import { avartarDto } from "./avatar.dto";
-import { Type } from "class-transformer";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class userDto {
    @IsString({ message: "닉네임은 문자열이어야 합니다." })
    @IsNotEmpty({ message: "닉네임은 빈 값이 아니어야 합니다." })
    readonly nickname: string;
-
-   @IsNotEmptyObject({ nullable: false }, { message: "캐릭터는 빈 값이 아니어야 합니다." })
-   @IsObject({ message: "캐릭터는 object 타입이어야 합니다." })
-   @ValidateNested() // 중첩된 객체에 대한 유효성 검사 활성화
-   @Type(() => avartarDto)
-   readonly avatar: avartarDto;
 }
