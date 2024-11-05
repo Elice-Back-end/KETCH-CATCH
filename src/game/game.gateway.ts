0bae032202f1661cf8622b67eab612a7baddc3be
[IoT3] 조수빈 <cdw3521@naver.com>
2024-11-05 21:25:11 +0900

    ⚰️ 불필요한 코드 제거 📝 response event명 수정

--- src/game/game.gateway.ts
+++ src/game/game.gateway.ts
@@ -1,21 +1,9 @@
-import {
-   OnGatewayConnection,
-   OnGatewayDisconnect,
-   SubscribeMessage,
-   WebSocketGateway,
-   MessageBody,
-   WebSocketServer,
-   ConnectedSocket,
-} from "@nestjs/websockets";
+import { SubscribeMessage, WebSocketGateway, MessageBody, WebSocketServer, ConnectedSocket } from "@nestjs/websockets";
 import { Socket, Server } from "socket.io";
 import { GameService } from "./game.service";
-import { Game_user } from "./game.interface";
-import { RoomSettingService } from "src/data/room/room-setting.service";
-import { Room } from "src/data/room/room.interface";
-import { UserService } from "src/data/user/user.service";
-import { User } from "src/data/user/user.interface";
 import { RoomService } from "src/room/room.service";
 import { GameState } from "./types/gameState";
+import { forwardRef, Inject } from "@nestjs/common";
 
 //채팅담당 게이트웨이
 @WebSocketGateway({
@@ -27,11 +15,7 @@ import { GameState } from "./types/gameState";
    },
 })
 export class ChatGateway {
-   constructor(
-      private userService: UserService,
-      private roomSettingService: RoomSettingService,
-      private gameService: GameService,
-   ) {}
+   constructor() {}
    // 서버를 선언해 브로드캐스트 환경
    @WebSocketServer()
    server: Server;
@@ -54,8 +38,8 @@ export class ChatGateway {
 })
 export class GameGateway {
    constructor(
+      @Inject(forwardRef(() => GameService))
       private gameService: GameService,
-      private roomSettingService: RoomSettingService,
       private roomService: RoomService,
    ) {}
 
@@ -69,8 +53,8 @@ export class GameGateway {
       // roomId 생성
       const [_, currentRoom] = Array.from(socket.rooms);
       // 초기 유저 설정
-      const Info = this.gameService.setDefaultGameInfo(socket.id, currentRoom); 
-      
+      const Info = this.gameService.setDefaultGameInfo(socket.id, currentRoom);
+
       // 게임시작 룸에게 전달데이터 메세지
       this.server.to(currentRoom).emit("started-game", Info);
       this.server.to(currentRoom).emit("notice", "게임이 시작되었습니다.");
@@ -81,42 +65,42 @@ export class GameGateway {
 
    // 게임 중 정답자가 나왔을 경우
    @SubscribeMessage("answer")
-   async collectPerson(@ConnectedSocket() socket: Socket, data: {idx: number}){
+   async collectPerson(@ConnectedSocket() socket: Socket, @MessageBody() data: { idx: number }) {
       // roomId 생성
       const [_, currentRoom] = Array.from(socket.rooms);
-      
+
       // 해당 idx를 가진 사람의 점수를 올려준다.
       const payload = this.gameService.plusScore(data.idx, currentRoom);
-      
-      // 룸에 유저정보 반환 
+
+      // 룸에 유저정보 반환
       this.server.to(currentRoom).emit("answer", payload);
    }
 
    // 다음라운드 넘어가기 버튼을 클릭했을 경우
    @SubscribeMessage("next-round")
-   async nextRound(@ConnectedSocket() socket: Socket){
+   async nextRound(@ConnectedSocket() socket: Socket) {
       // roomId 생성
       const [_, currentRoom] = Array.from(socket.rooms);
-      
+
       // 룸에게 다음 라운드로 가라고 지시한다.
       this.gameService.nextRound(currentRoom);
    }
 
    // 게임 중 유저가 퇴장했을시
    @SubscribeMessage("in-game-exit")
-   inGameExit(@ConnectedSocket() socket: Socket){
+   inGameExit(@ConnectedSocket() socket: Socket) {
       // roomId, users, nickname 가져옴 / 예외처리
       const { roomId, users, nickname } = this.roomService.exitRoom(socket);
       if (roomId === undefined || users === undefined || nickname === undefined) return;
-      
+
       // 유저이탈이벤트
-      const gameData = this.gameService.inGameExit(socket ,roomId);
+      const gameData = this.gameService.inGameExit(socket, roomId);
 
       const payload = {
          users: users,
          participants: gameData.participants,
-         answwerUser: gameData.answerUser
-      }
+         answwerUser: gameData.answerUser,
+      };
 
       const eventName = users.gameState === GameState.Pending ? "pending-room" : "game-room";
       this.server.to(roomId).emit("notice", { message: `${nickname}님이 방을 나갔습니다.` });
@@ -125,7 +109,7 @@ export class GameGateway {
 
    // 게임 종료
    @SubscribeMessage("finish-game")
-   finishGame(@ConnectedSocket() socket: Socket){
+   finishGame(@ConnectedSocket() socket: Socket) {
       // roomId 생성
       const [_, currentRoom] = Array.from(socket.rooms);
 
@@ -134,7 +118,7 @@ export class GameGateway {
 
       // 룸에게 유저정보 반환
       this.server.to(currentRoom).emit("finished-game", payload);
-   } 
+   }
 
    // 게임 종료 후 HOME
    @SubscribeMessage("go-to-home")
@@ -160,6 +144,6 @@ export class GameGateway {
    }
 
    emitEvent(roomId: string, data: any) {
-      this.server.to(roomId).emit('eventName', data);
-    }
+      this.server.to(roomId).emit("next-round", data);
+   }
 }
