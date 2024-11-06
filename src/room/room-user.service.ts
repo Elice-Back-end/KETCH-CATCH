d32f8fba9b2c31e6706b3541af063b130e52a783
[IoT3] 조수빈 <cdw3521@naver.com>
2024-11-06 20:18:44 +0900

    ✏️ 데이터 가공 작업

--- src/room/room-user.service.ts
+++ src/room/room-user.service.ts
@@ -15,13 +15,13 @@ export class RoomUserService {
       const foundUsers = this.userService.findUsers(roomId);
       // 대기 방 안에 있는 유저들
       const users = foundUsers.map((user) => {
-         return { id: count++, name: user.nickname, score: 0 };
+         return { id: count++, nickName: user.nickname, score: 0 };
       });
       const foundRoom = this.roomSettingService.findOneRoom(roomId);
       // client에게 보낼 데이터
       const payload = {
          gameState: foundRoom.isStart ? GameState.Playing : GameState.Pending,
-         code: `${process.env.INVITATION_CODE}${roomId}`,
+         code: roomId,
          users,
          host: users[0].id,
          drawer: users[0].id,
